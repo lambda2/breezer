31ba7b3420699afc50a487694d5e35ee7e673928
André Aubin <andre@pandascore.co>
2020-02-05 15:15:40 +0100

    lob

--- lib/breezer.rb
+++ lib/breezer.rb
@@ -1,6 +1,12 @@
 class Breezer
   def self.freeze!(gemfile_path, lockfile_path, **options)
-    deps = Parser.deps(lockfile_path, options)
+    absolute_lockfile_path = File.join(File.dirname(__FILE__), '..', lockfile_path)
+    absolute_gemfile_path = File.join(File.dirname(__FILE__), '..', gemfile_path)
+    ENV['BUNDLE_GEMFILE'] = absolute_gemfile_path
+    puts "absolute_lockfile_path: #{absolute_lockfile_path}"
+    puts "absolute_gemfile_path: #{absolute_gemfile_path}"
+    puts ENV
+    # deps = Parser.deps(absolute_lockfile_path, options)
   end
 end
 
