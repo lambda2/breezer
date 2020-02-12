b34b8774d01fa88719436144fe2c923d614ca0cc
André Aubin <andre@pandascore.co>
2020-02-12 17:07:50 +0100

    plugins

--- plugins.rb
+++ plugins.rb
@@ -5,19 +5,10 @@ require 'breezer'
 
 # This is the plugin for bundler
 class Breezer < Bundler::Plugin::API
-  # Register this class as a handler for the `my_command` command
-  command 'freeze'
+  command 'breeze'
 
-  # The exec method will be called with the `command` and the `args`.
-  # This is where you should handle all logic and functionality
   def exec(_command, args)
     r = Breezer::Command.run!(args)
     exit(r == false ? 2 : 0)
   end
 end
-
-# #!/usr/bin/env ruby
-
-
-# r = Breezer::Command.run!(ARGV)
-# exit(r == false ? 2 : 0)
